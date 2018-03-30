4572ecc559b3439ffb1b34ee0812a22792430f2b
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-03-29 18:42:56 -0700

    all tests passing

--- lib/encode_repeating.rb
+++ lib/encode_repeating.rb
@@ -1,6 +1,31 @@
 # A method that updates the string by replacing consecutive repeating characters
 # with a number representing the frequency. The replacement is done only if the
 # string length will get reduced by this process.
+
 def encode_repeating(my_string)
-  raise NotImplementedError
+
+  return nil if my_string.nil?
+
+  start = 0
+
+  until my_string[start].nil?
+
+    count = 1
+
+    while my_string[start + count] == my_string[start]
+      count += 1
+    end
+
+    if count < 3
+      start += count
+    else
+      my_string[start + 1] = count.to_s
+      my_string.slice!(start + 2, count - 2)
+      start += 2
+    end
+
+  end
+
+  return my_string
+
 end
